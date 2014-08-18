6bd9cd50dad95274be2783dd12c5ca6f4b8b82ae
itang <live.tang@gmail.com>
2014-08-18 13:52:37 +0800

    update

--- install-go.sh
+++ install-go.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-(
-rm go1.3.linux-amd64.tar.gz
-wget http://golang.org/dl/go1.3.linux-amd64.tar.gz
-tar zxvf go1.3.linux-amd64.tar.gz -C ~/dev-env
+go version;
 
-go version
+VERSION=1.3.1
+URL=http://golangtc.com/static/go/go${VERSION}.linux-amd64.tar.gz
+
+echo "download from ${URL}"
+
+(
+cd /tmp
+wget $URL && 
+rm -rf ~/dev-env/go && 
+tar zxvf go${VERSION}.linux-amd64.tar.gz -C ~/dev-env &&
+go version;
 )
+
