516d4ee4818105deed89d5e563cfc0da33f4a666
itang <live.tang@gmail.com>
2014-09-09 15:02:19 +0800

    update install gradle shell

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,7 +1,15 @@
+VERSION=2.1
 (
 cd /tmp
-wget https://downloads.gradle.org/distributions/gradle-2.0-all.zip &&
-unzip gradle-2.0-all.zip -d ~/dev-env &&
-cd /dev-env &&
-ln -s gradle-2.0 gradle
+
+if [ ! -f gradle-${VERSION}-all.zip ]; then
+   wget https://downloads.gradle.org/distributions/gradle-${VERSION}-all.zip
+fi
+ 
+ls gradle-${VERSION}-all.zip &&
+unzip gradle-${VERSION}-all.zip -d ~/dev-env &&
+cd ~/dev-env &&
+rm -rf gradle &&
+ln -s gradle-2.1 gradle &&
+gradle -v
 )
