52f18b5af6bc509e0f11fbde752295f1a1fd7aec
itang <live.tang@gmail.com>
2014-09-18 21:10:12 +0800

    download rust use http

--- install-rust.sh
+++ install-rust.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+SH_FILE=rustup.sh
 # export http_proxy=localhost:8087
-curl -s https://static.rust-lang.org/rustup.sh | sudo sh
-rustc --version
+# curl -s https://static.rust-lang.org/rustup.sh |   sudo sh
+(
+cd /tmp
+curl -s http://static.rust-lang.org/rustup.sh -O $SH_FILE &&
+sed -i "s/https/http/g" $SH_FILE # just use http
+
+if [ "$?" -ne "0" ]; then 
+    echo "replace error"
+    cat -n $SH_FILE  | grep http  
+else
+    # export http_proxy=localhost:8087
+    sudo -E bash $SH_FILE && 
+    rustc --version
+fi 
+)
 
