68a2c80f2bd8f5d136ef95872a62e0558736e443
itang <live.tang@gmail.com>
2014-09-18 21:26:19 +0800

    u

--- install-rust.sh
+++ install-rust.sh
@@ -3,12 +3,19 @@
 SH_FILE=rustup.sh
 # export http_proxy=localhost:8087
 # curl -s https://static.rust-lang.org/rustup.sh |   sudo sh
+
 (
 cd /tmp
-curl -s http://static.rust-lang.org/rustup.sh -O $SH_FILE &&
-sed -i "s/https/http/g" $SH_FILE # just use http
+curl http://static.rust-lang.org/rustup.sh -o $SH_FILE 
 
-if [ "$?" -ne "0" ]; then 
+if [ "$?" != "0" ]; then
+  echo "下载rustup.sh失败"
+  exit -1
+fi
+
+sed -i "s/https/http/g" $SH_FILE
+
+if [ "$?" != "0" ]; then 
     echo "replace error"
     cat -n $SH_FILE  | grep http  
 else
@@ -16,5 +23,7 @@ else
     sudo -E bash $SH_FILE && 
     rustc --version
 fi 
+
+exit 0
 )
 
