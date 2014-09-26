9da4920aea527373258cb7d436d850bbcb677fd9
itang <live.tang@gmail.com>
2014-09-26 21:03:01 +0800

    node-0.10.32

--- install-node.sh
+++ install-node.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VERSION=0.10.31
+VERSION=0.10.32
 
 (
 cd /tmp
 rm node-v${VERSION}-linux-x64.tar.gz
-wget http://nodejs.org/dist/v0.10.31/node-v${VERSION}-linux-x64.tar.gz &&
+wget http://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.gz &&
 rm -rf ~/dev-env/node-v${VERSION}-linux-x64 &&
 tar zxvf node-v${VERSION}-linux-x64.tar.gz -C ~/dev-env &&
 cd ~/dev-env && 
