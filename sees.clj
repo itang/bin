48baf00be7f056c4b1bc3c374e7b3dbf149365ec
itang <live.tang@gmail.com>
2014-10-10 14:12:01 +0800

    u

--- sees.clj
+++ sees.clj
@@ -3,12 +3,13 @@
 (require '[clojure.java.browse :as browse])
 
 (def sees ["https://twitter.com/"
-            "http://www.reddit.com/r/rust"
-            "https://github.com/stars?direction=desc&sort=updated"
-            "http://getprismatic.com/"
-            "http://www.douban.com"
-            "http://www.weibo.com"
-            "http://bbs.szhome.com/30017.html"])
+           "http://www.reddit.com/r/rust"
+           "https://github.com/stars?direction=desc&sort=updated"
+           "http://getprismatic.com/"
+           "http://www.douban.com"
+           "http://www.weibo.com"
+           "http://www.deftype.com"
+           "http://bbs.szhome.com/30017.html"])
 
 (doseq [see (reverse sees)]
   (browse/browse-url see))
