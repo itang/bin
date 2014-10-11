e142b3f4a8edeb4683a82871a78ec6850cb08b87
itang <live.tang@gmail.com>
2014-10-11 20:52:48 +0800

    elixir-1.0.1

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-VERSION=1.0.0
+VERSION=1.0.1
 (
 cd /tmp
 rm elixir.zip
 
-wget https://github.com/elixir-lang/elixir/archive/v1.0.0.zip -O elixir.zip &&
+wget https://github.com/elixir-lang/elixir/archive/v${VERSION}.zip -O elixir.zip &&
 rm ~/dev-env/elixir
 
 unzip elixir.zip -d ~/dev-env/ && 
 cd ~/dev-env &&
-ln -s elixir-1.0.0 elixir &&
+ln -s elixir-${VERSION} elixir &&
 cd elixir &&
 make 
 
