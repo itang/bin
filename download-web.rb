1ece63e86c9bc9613c085c4db6e6695eb793d828
itang <live.tang@gmail.com>
2015-02-13 12:20:45 +0800

    cleanup

--- download-web.rb
+++ download-web.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 
 site = ARGV[0]
-dir = site.sub('http://', '').gsub(/[\/\.]/, '_')
-
-cmd = "wget --mirror --limit-rate=100k --wait=1 -erobots=off --no-parent --page-requisites --convert-links --no-host-directories --cut-dirs=2 --directory-prefix=#{dir} #{site}"
-
-puts cmd
-system cmd
+if site.nil? || site.empty?
+  puts "download-web.rb http://xxxx.com/xxxx"
+else
+  dir = site.sub('http://', '').gsub(/[\/\.]/, '_')
+  cmd = "wget --mirror --limit-rate=100k --wait=1 -erobots=off --no-parent --page-requisites --convert-links --no-host-directories --cut-dirs=2 --directory-prefix=#{dir} #{site}".tap &method(:puts)
+  system cmd
+end
