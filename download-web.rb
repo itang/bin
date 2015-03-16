af322c38103414db08b4542df9a2709284cba9bc
itang <live.tang@gmail.com>
2015-03-16 12:46:10 +0800

    process https

--- download-web.rb
+++ download-web.rb
@@ -4,7 +4,7 @@ site = ARGV[0]
 if site.nil? || site.empty?
   puts "download-web.rb http://xxxx.com/xxxx"
 else
-  dir = site.sub('http://', '').gsub(/[\/\.]/, '_')
+  dir = site.sub(/http.?:\/\//, '').gsub(/[\/\.]/, '_')
   cmd = "wget --mirror --limit-rate=100k --wait=1 -erobots=off --no-parent --page-requisites --convert-links --no-host-directories --cut-dirs=2 --no-check-certificate --directory-prefix=#{dir} #{site}".tap &method(:puts)
   system cmd
 end
