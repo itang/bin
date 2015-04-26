66a92b51fe6fb2fe44f15801cb469a8bc52ab406
itang <live.tang@gmail.com>
2015-04-26 16:17:31 +0800

    update versions

--- install-erlang.sh
+++ install-erlang.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION=17.3-1
+VERSION=18.0-rc1-1
 
 (
 cd /tmp
-wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_${VERSION}~ubuntu~precise_amd64.deb &&
+wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_3_general/esl-erlang_${VERSION}~ubuntu~precise_amd64.deb
 sudo dpkg -i esl-erlang_${VERSION}~ubuntu~precise_amd64.deb &&
 erl -version
 )
--- install-rust.sh
+++ install-rust.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-SH_FILE=rustup.sh
-# export http_proxy=localhost:8087
-# curl -s https://static.rust-lang.org/rustup.sh |   sudo sh
+curl -s https://static.rust-lang.org/rustup.sh | sudo sh -s -- --channel=nightly
 
-(
-cd /tmp
-curl http://static.rust-lang.org/rustup.sh -o $SH_FILE 
-
-if [ "$?" != "0" ]; then
-  echo "下载rustup.sh失败"
-  exit -1
-fi
-
-sed -i "s/https/http/g" $SH_FILE
-
-if [ "$?" != "0" ]; then 
-    echo "replace error"
-    cat -n $SH_FILE  | grep http  
-else
-    # export http_proxy=localhost:8087
-    sudo -E bash $SH_FILE && 
-    rustc --version
-fi 
-
-exit 0
-)
-
+rustc --version
