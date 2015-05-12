c3ab1609d0697328d5075f0af197b1e800ddce44
itang <live.tang@gmail.com>
2015-05-12 11:30:11 +0800

    common gems

--- install-rvm.sh
+++ install-rvm.sh
@@ -4,8 +4,26 @@
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
 
+
+##################
 ruby -v
 
+gem sources --remove https://rubygems.org/
+gem sources -a https://ruby.taobao.org/
+gem sources -l
+
+gem install redis
+gem install edn
+gem install roo
+gem install rubocop 
+gem install zookeeper
+
+sudo apt-get install libmysqlclient-dev  
+gem install mysql2
+
+gem install ruby-protocol-buffers
+gem install buildr  
+##################
 rvm install jruby
 
 rvm use jruby
