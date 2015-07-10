f002bbe58cf9676887728291ed12898867a464f4
itang <live.tang@gmail.com>
2015-07-10 13:09:36 +0800

    u

--- install-node.sh
+++ install-node.sh
@@ -4,9 +4,9 @@ VERSION=2.3.3
 
 (
 cd /tmp
-#rm iojs-v${VERSION}-linux-x64.tar.xz
-#wget https://iojs.org/dist/v${VERSION}/iojs-v${VERSION}-linux-x64.tar.xz &&
-rm -rf ~/dev-env/iojs-v${VERSION}-linux-x64 &&
+rm iojs-v${VERSION}-linux-x64.tar.xz
+rm -rf ~/dev-env/iojs-v${VERSION}-linux-x64
+wget https://iojs.org/dist/v${VERSION}/iojs-v${VERSION}-linux-x64.tar.xz &&
 tar xvJf iojs-v${VERSION}-linux-x64.tar.xz -C ~/dev-env &&
 cd ~/dev-env && 
 rm -rf node &&
---
+++ install-phoenix.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+VERSION=0.14.0
+mix archive.install https://github.com/phoenixframework/phoenix/releases/download/v${VERSION}/phoenix_new-${VERSION}.ez
