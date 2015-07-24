f46c6eecd429d64cd410be4e28b4779a5805c775
itang <live.tang@gmail.com>
2015-07-24 18:16:11 +0800

    u

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,15 +1,15 @@
-VERSION=2.1
+VERSION=2.5
 (
 cd /tmp
 
 if [ ! -f gradle-${VERSION}-all.zip ]; then
    wget https://downloads.gradle.org/distributions/gradle-${VERSION}-all.zip
 fi
- 
+
 ls gradle-${VERSION}-all.zip &&
 unzip gradle-${VERSION}-all.zip -d ~/dev-env &&
 cd ~/dev-env &&
 rm -rf gradle &&
-ln -s gradle-2.1 gradle &&
+ln -s gradle-${VERSION} gradle &&
 gradle -v
 )
