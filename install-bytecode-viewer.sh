a1c02196e31927671d69f269cf9d995f5f97caac
itang <live.tang@gmail.com>
2015-08-07 11:30:43 +0800

    bytecode viewer

---
+++ install-bytecode-viewer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=2.9.8
+
+cd /tmp
+wget https://github.com/Konloch/bytecode-viewer/releases/download/v$VERSION/BytecodeViewer.$VERSION.zip
+
+rm ~/dev-env/bytecodeviewer
+mkdir ~/dev-env/bytecodeviewer && unzip BytecodeViewer.$VERSION.zip -d ~/dev-env/bytecodeviewer
+
+echo "#!/bin/bash
+
+java -jar "\"$HOME/dev-env/bytecodeviewer/BytecodeViewer ${VERSION}.jar\""
+" > ~/dev-env/tools/viewcodeviewer
+
+chmod +x ~/dev-env/tools/viewcodeviewer
