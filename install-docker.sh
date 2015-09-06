013a78aefad6c1dc405b958324c60fbb999b59f7
itang <live.tang@gmail.com>
2015-09-06 19:37:36 +0800

    update install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # link(http://docs.docker.com/installation/ubuntulinux/#ubuntu-trusty-1404-lts-64-bit)
-curl -sSL https://get.docker.io/ubuntu/ | sudo sh &&
+echo "wget -qO- https://get.docker.com/gpg | sudo apt-key add -"
+wget -qO- https://get.docker.com/ | sh
 docker --version;
