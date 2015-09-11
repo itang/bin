4a0feeff073c5d1e8c87d1fa465789f57772f11f
itang <live.tang@gmail.com>
2015-09-11 11:32:53 +0800

    fix elixir install

--- install-elixir.sh
+++ install-elixir.sh
@@ -5,7 +5,9 @@ VERSION=1.1.0-beta
 cd /tmp
 rm Precompiled.zip
 
-wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm ~/dev-env/elixir && mkdir -p ~/dev-env/elixir
+wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip
+rm -rf ~/dev-env/elixir 
+mkdir -p ~/dev-env/elixir
 
 unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
 )
