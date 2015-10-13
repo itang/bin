6a27c0c36f6fa709471892bad030f88f168ec682
itang <live.tang@gmail.com>
2015-10-13 11:11:51 +0800

    use node-v4.2.0

--- install-node.sh
+++ install-node.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-VERSION=3.0.0
+VERSION=4.2.0
 
 (
 cd /tmp
-rm iojs-v${VERSION}-linux-x64.tar.xz
-rm -rf ~/dev-env/iojs-v${VERSION}-linux-x64
-wget https://iojs.org/dist/v${VERSION}/iojs-v${VERSION}-linux-x64.tar.xz &&
-tar xvJf iojs-v${VERSION}-linux-x64.tar.xz -C ~/dev-env &&
-cd ~/dev-env &&
-rm -rf node &&
-ln -s iojs-v${VERSION}-linux-x64 node &&
-node -v
+rm node-v${VERSION}-linux-x64.tar.gz
+wget https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.gz &&
+tar zxvf node-v${VERSION}-linux-x64.tar.gz -C ~/dev-env/ && 
+cd ~/dev-env/ &&
+rm node &&
+ln -s node-v${VERSION}-linux-x64 node
+
+node -v 
+
 ##&&
 #npm install -g react-tools
 )
