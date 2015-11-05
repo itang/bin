3bcb7cbcf821a7d107623f365e9b2d805f74b38b
itang <live.tang@gmail.com>
2015-11-05 11:12:27 +0800

    u

---
+++ install-httpie.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install python-pip
+
+# sudo apt-get install httpie
+
+#The latest development version can be installed directly from GitHub
+sudo pip install --upgrade https://github.com/jkbrzt/httpie/tarball/master
--- install-node.sh
+++ install-node.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-VERSION=4.2.1
+VERSION=5.0.0
 
 (
 cd /tmp
 rm node-v${VERSION}-linux-x64.tar.gz
 wget https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-linux-x64.tar.gz &&
-tar zxvf node-v${VERSION}-linux-x64.tar.gz -C ~/dev-env/ && 
+tar zxvf node-v${VERSION}-linux-x64.tar.gz -C ~/dev-env/ &&
 cd ~/dev-env/ &&
 rm node &&
 ln -s node-v${VERSION}-linux-x64 node
 
-node -v 
+node -v
 
 ##&&
 #npm install -g react-tools
