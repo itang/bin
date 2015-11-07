0ae8ebf69c677299f17d1334b9cfb3e5908296a4
itang <live.tang@gmail.com>
2015-11-07 10:53:37 +0800

    add install kotlin shell

---
+++ install-kotlin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+VERSION=1.0.0-beta-1103
+
+cd /tmp
+wget https://github.com/JetBrains/kotlin/releases/download/build-$VERSION/kotlin-compiler-$VERSION.zip 
+
+rm -rf ~/dev-env/kotlinc
+
+unzip kotlin-compiler-$VERSION.zip -d ~/dev-env/
+
+cd ~/dev-env
