25cd4256486b9b948fcb43c4ac8a0d728e2040cf
itang <live.tang@gmail.com>
2015-11-16 13:35:10 +0800

    update rust shell

--- install-rust.sh
+++ install-rust.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 #curl -sSf https://static.rust-lang.org/rustup.sh | sh -s -- --channel=nightly
-curl -sf -L https://static.rust-lang.org/rustup.sh | sh
+#curl -sf -L https://static.rust-lang.org/rustup.sh | sh
+
+#rustc --version
+
+
+curl -sf https://raw.githubusercontent.com/brson/multirust/master/blastoff.sh | sh
+
+multirust update nightly
+
+multirust update stable
+
+multirust default nightly
 
 rustc --version
+
+cargo install cargo-extras
+cargo install cargo-add
+
+
