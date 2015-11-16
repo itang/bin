6c6a4866326a5a4a9f91c85cb2a04abc614aeb72
itang <live.tang@gmail.com>
2015-11-16 23:42:19 +0800

    add install frege shells

---
+++ install-frege-repl.sh
@@ -0,0 +1,18 @@
+  #!/bin/bash
+
+VERSION=1.4-SNAPSHOT
+
+cd /tmp
+
+wget https://github.com/Frege/frege-repl/releases/download/${VERSION}/frege-repl-${VERSION}.zip
+
+rm -rf ~/dev-env/frege-repl-${VERSION}
+
+unzip frege-repl-${VERSION}.zip -d ~/dev-env/
+
+cd ~/dev-env
+
+rm frege-repl
+
+ln -s frege-repl-${VERSION} frege-repl
+
