f3a2f4fcc27cf7d334552d5d0b677c6e481c969e
itang <live.tang@gmail.com>
2015-11-21 12:16:59 +0800

    amm-0.4.9

--- install-amm.sh
+++ install-amm.sh
@@ -1,5 +1,5 @@
 cd ~/dev-env/tools
 
-mkdir ~/.ammonite; curl -L -o ~/.ammonite/predef.scala http://git.io/vnnBy
+mkdir -p ~/.ammonite; curl -L -o ~/.ammonite/predef.scala http://git.io/v4y4p
 
-curl -L -o amm http://git.io/vnnBX; chmod +x amm; ./amm
+curl -L -o amm http://git.io/v4gOX; chmod +x amm; ./amm
