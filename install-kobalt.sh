b8ba741a178961096972543b092bc7b53edca5f5
itang <live.tang@gmail.com>
2015-12-03 15:45:58 +0800

    code, kobalt

---
+++ install-kobalt.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSION=0.314
+
+function download() {
+    cd /tmp
+    wget https://github.com/cbeust/kobalt/releases/download/$VERSION/kobalt-$VERSION.zip
+}
+
+function extract() {
+    cd /tmp
+    rm -rf /home/itang/dev-env/kobalt
+    mkdir -p /home/itang/dev-env/kobalt
+    unzip kobalt-$VERSION.zip -d /home/itang/dev-env/kobalt/
+}
+
+function install() {
+    cd /home/itang/dev-env/kobalt
+    chmod +x kobaltw
+    tree
+    #cd ~/dev-env
+    #ln -sfn kobalt-$VERSION kobalt
+}
+
+##################################################################################
+download && extract && install
--- install-storm.sh
+++ install-storm.sh
@@ -5,7 +5,9 @@ VERSION=0.10.0
 cd /tmp
 
 rm apache-storm-${VERSION}/apache-storm-${VERSION}.tar.gz
-wget http://apache.fayea.com/storm/apache-storm-${VERSION}/apache-storm-${VERSION}.tar.gz
+
+wget http://mirror.bit.edu.cn/apache/storm/apache-storm-${VERSION}/apache-storm-${VERSION}.tar.gz
+#wget http://apache.fayea.com/storm/apache-storm-${VERSION}/apache-storm-${VERSION}.tar.gz
 
 tar zxvf apache-storm-${VERSION}.tar.gz -C ~/dev-env/
 
