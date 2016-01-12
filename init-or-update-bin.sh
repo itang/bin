431269e279e9ed884f8318b8d22beb8ef3ea95a1
itang <live.tang@gmail.com>
2016-01-12 16:42:21 +0800

    more

--- init-or-update-bin.sh
+++ init-or-update-bin.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 
-sudo apt-get install -y --force-yes build-essential 
+sudo apt-get install -y --force-yes build-essential
 #sudo apt-get install -y --force-yes ubuntu-restricted-extras
-sudo apt-get install -y --force-yes wget curl git mercurial xclip cloc tree p7zip-full rsync inotify-tools
+sudo apt-get install -y --force-yes wget curl git mercurial xclip cloc tree p7zip-full rsync inotify-tools tmux
 
 # SBT
 echo "install sbt"
 curl -s https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt > ~/bin/sbt && chmod 0755 ~/bin/sbt
 
-# lein 
+# lein
 curl -s https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin/lein && chmod 0755 ~/bin/lein
 
