794bae80c91ae0ba388b7ccbf174ff46c2573ea2
itang <live.tang@gmail.com>
2016-01-12 15:29:03 +0800

    add gems

---
+++ install-gems.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## change gem sources
+gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
+gem sources -l
+
+######################################
+## lang enhance.
+gem install rubocop
+gem install pry pry-doc
+
+## languages.
+gem install typescript
+
+
+## tools.
+gem install roo
+gem install ruby-protocol-buffers
+gem install livereload
+gem install guard guard-shell guard-livereload
+
+## libs.
+sudo apt-get install libmysqlclient-dev
+gem install mysql2
+gem install zookeeper
+gem install redis
