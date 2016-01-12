0549daca77a30da88e23556b681b2d16720b7b51
itang <live.tang@gmail.com>
2016-01-12 16:40:39 +0800

    npms

---
+++ install-npms.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+
+npm install -g source-map-support  --registry=https://registry.npm.taobao.org
+
+cnpm install -g jshint
+
+npm install -g typescript
