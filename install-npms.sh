8bdafd904cbe044079162a16afc763ff7bfa21d5
itang <live.tang@gmail.com>
2016-01-13 17:01:21 +0800

    more

--- install-npms.sh
+++ install-npms.sh
@@ -7,4 +7,7 @@ npm install -g source-map-support  --registry=https://registry.npm.taobao.org
 cnpm install -g jshint
 
 npm install -g typescript
+# tsun
 npm install -g tsun
+# tsfmt
+npm install -g typescript-formatter
