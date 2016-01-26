8c7110872867ebec1e3d3fbdba1263329f6c8147
itang <live.tang@gmail.com>
2016-01-26 17:46:20 +0800

    add install vertx shell

---
+++ install-vertx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+version=3.2.0
+
+(
+cd /tmp
+wget https://bintray.com/artifact/download/vertx/downloads/vert.x-$version.tar.gz
+
+mkdir -p ~/dev-env/vert.x-$version
+tar zxvf vert.x-$version.tar.gz -C ~/dev-env/vert.x-$version
+
+cd ~/dev-env/
+ln -sfn vert.x-$version vertx
+)
+
+vertx -version
