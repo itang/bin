13bcf44db1e6ea609bd2ec9e5399572268a4df08
itang <live.tang@gmail.com>
2016-02-14 10:10:25 +0800

    cleanup

--- install-rkt.sh
+++ install-rkt.sh
@@ -5,13 +5,13 @@ VERSION=1.0.0
 (
     cd /tmp
 
-    wget https://github.com/coreos/rkt/releases/download/v$VERSION/rkt-v$VERSION.tar.gz
+    #wget https://github.com/coreos/rkt/releases/download/v$VERSION/rkt-v$VERSION.tar.gz
 
     tar xfv rkt-v$VERSION.tar.gz -C ~/dev-env/
 
     cd ~/dev-env
 
-    ln -sfn rtk-v$VERSION rkt
+    ln -sfn rkt-v$VERSION rkt
 
     alias rkt="sudo '/home/itang/dev-env/rkt/rkt'"
 )
