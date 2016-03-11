1f6b078aec50f8fd45791643ff470216ded4d3f4
itang <live.tang@gmail.com>
2016-03-11 15:05:42 +0800

    add install CBT shell

---
+++ install-cbt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/dev-env
+
+if [[ -d "cbt" ]]
+then
+  echo update
+  (cd cbt; git pull)
+else
+  echo "git clone https://github.com/cvogt/cbt.git"
+  git clone https://github.com/cvogt/cbt.git
+fi
+
