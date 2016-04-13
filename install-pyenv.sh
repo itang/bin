cd6666157c299fd8fdac176116c29707cbdda7ef
itang <live.tang@gmail.com>
2016-04-13 18:01:42 +0800

    m

---
+++ install-pyenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv --depth 1
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
