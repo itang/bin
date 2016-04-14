5fb65dc821616c99dbafe5d58e01e596db1dbd4e
itang <live.tang@gmail.com>
2016-04-14 21:14:54 +0800

    u

--- install-zsh.sh
+++ install-zsh.sh
@@ -20,3 +20,8 @@ curl -L http://install.ohmyz.sh | sh
 chsh -s $(which zsh)
 
 #source ${ZDOTDIR:-${HOME}}/.zlogin
+
+# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+#
+
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
