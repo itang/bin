41342c017475c704d5016679cd336a6883b7a5d3
itang <live.tang@gmail.com>
2016-04-23 14:36:48 +0800

    update

--- install-postgresql.sh
+++ install-postgresql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #sudo apt-get install postgresql-9.3 pgadmin3
-sudo apt-get install postgresql-9.3 pgadmin3
+sudo apt-get install postgresql-9.4 pgadmin3 postgresql-server-dev-all
 
 # sudo pg_createcluster 9.3 main --start
