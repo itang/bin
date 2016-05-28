122288916c23c49b5a5905c2c83c0250ed89163a
itang <live.tang@gmail.com>
2016-05-28 20:21:01 +0800

    u

--- install-amm.sh
+++ install-amm.sh
@@ -1,4 +1,5 @@
 cd ~/dev-env/tools
 
-mkdir ~/.ammonite; curl -L -o ~/.ammonite/predef.scala https://git.io/vVfyF
-curl -L -o amm https://git.io/vVfy5; chmod +x amm; ./amm
+mkdir -p ~/.ammonite; curl -L -o ~/.ammonite/predef.scala https://git.io/vro0a
+curl -L -o amm https://git.io/vro0u && chmod +x amm && ./amm
+
