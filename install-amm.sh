9a1fc17f69417d6f3ee45f658580a8d85b454984
itang <live.tang@gmail.com>
2016-06-19 21:50:12 +0800

    update

--- install-amm.sh
+++ install-amm.sh
@@ -1,5 +1,5 @@
 cd ~/dev-env/tools
 
-mkdir -p ~/.ammonite; curl -L -o ~/.ammonite/predef.scala https://git.io/vro0a
-curl -L -o amm https://git.io/vro0u && chmod +x amm && ./amm
+mkdir ~/.ammonite && curl -L -o ~/.ammonite/predef.scala https://git.io/voluk
+curl -L -o amm https://git.io/voluT && chmod +x amm && ./amm
 
--- install-pony.sh
+++ install-pony.sh
@@ -8,8 +8,16 @@
 
 #sudo apt-get install ponyc
 
+mode=$1
 
 cd ~/dev-env
+
+if [ "$mode" == 'update' ]; then
+
+  cd ponyc
+  git pull
+
+else
 
 rm -rf ponyc
 git clone git@github.com:CausalityLtd/ponyc.git --depth 1
@@ -19,5 +27,7 @@ cd ponyc
 sudo apt-get install build-essential git llvm-dev \
                        zlib1g-dev libncurses5-dev libssl-dev
 # sudo apt-get install llvm-3.6
-make config=release
 
+fi
+
+make config=release
