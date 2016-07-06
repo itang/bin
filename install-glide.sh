7bd4d1ffed41c57714d59e0f6a01035bdf538aed
itang <live.tang@gmail.com>
2016-07-06 20:36:47 +0800

    install glide from master source

--- install-glide.sh
+++ install-glide.sh
@@ -5,13 +5,30 @@ VERSION=0.10.2
 
 glide -v
 
-(
-    cd /tmp
-    wget https://github.com/Masterminds/glide/releases/download/$VERSION/glide-$VERSION-linux-amd64.tar.gz
+#(
+#    cd /tmp
+#    wget https://github.com/Masterminds/glide/releases/download/$VERSION/glide-$VERSION-linux-amd64.tar.gz
 
-    tar zxvf glide-$VERSION-linux-amd64.tar.gz
+#    tar zxvf glide-$VERSION-linux-amd64.tar.gz
 
-    cp linux-amd64/glide ~/dev-env/tools
-)
+#    cp linux-amd64/glide ~/dev-env/tools
+#)
+
+action=$1
+
+if [ "$action" == 'update' ]; then
+  
+  cd $GOPATH/src/github.com/Masterminds/glide
+  git pull
+
+else
+  mkdir -p $GOPATH/src/github.com/Masterminds
+  cd $GOPATH/src/github.com/Masterminds
+  git clone git@github.com:Masterminds/glide.git --depth 1
+fi
+
+cd $GOPATH/src/github.com/Masterminds/glide
+make build
+cp glide ~/dev-env/tools/
 
 glide -v
