82aaaa9686398a0ffa377f1e13e756c4ff675e23
itang <live.tang@gmail.com>
2016-08-19 20:23:28 +0800

    cleanup

--- install-amm.sh
+++ install-amm.sh
@@ -1,5 +1,5 @@
 cd ~/dev-env/tools
 
 mkdir -p ~/.ammonite && curl -L -o ~/.ammonite/predef.sc https://git.io/vo4wx
-curl -L -o amm https://git.io/vKSbm && chmod +x amm && ./amm
+curl -L -o amm  https://git.io/vKSOR && chmod +x amm && ./amm
 
