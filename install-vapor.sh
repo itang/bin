56f2ae16e603e940ff6daa4104d7863a1c4497ad
itang <live.tang@gmail.com>
2016-09-16 16:31:07 +0800

    u

--- install-vapor.sh
+++ install-vapor.sh
@@ -3,21 +3,11 @@
 
 action=$1
 
-
 if [ "$action" == "update" ]; then
-
     vapor self update
-    vapor self compile
-
+    # vapor self compile
 else
-
-    cd ~/dev-env/tools
-
-    curl -L cli.qutheory.io -o vapor
-    chmod +x vapor
-    
-    vapor self compile
+    curl -sL toolbox.vapor.sh | bash
 fi
 
-vapor help
-
+vapor --help
