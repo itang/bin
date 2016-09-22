2a4b4ebfd642898f8d0c6dfa6633002fb33e55ae
itang <live.tang@gmail.com>
2016-09-22 21:58:45 +0800

    u elixir

--- install-erlang.sh
+++ install-erlang.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-VERSION=18.3-1~ubuntu~wily_amd64
+VERSION=19.0.3-1~ubuntu~wily_amd64
 
 (
-cd /tmp
-
-wget http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_$VERSION.deb
-
-
-sudo dpkg -i esl-erlang_$VERSION.deb && erl -version
+    cd /tmp
+    wget http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_$VERSION.deb && 
+    sudo dpkg -i esl-erlang_$VERSION.deb && 
+    erl -version
 )
