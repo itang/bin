c9b7f8125e75863c139f86e7809559346ae78bbb
itang <live.tang@gmail.com>
2016-10-08 20:26:42 +0800

    new elm

--- install-amm.sh
+++ install-amm.sh
@@ -3,6 +3,4 @@
 cd ~/dev-env/tools
 
 mkdir -p ~/.ammonite && curl -L -o ~/.ammonite/predef.sc https://git.io/v6r5y
-curl -L -o amm https://git.io/v6KE6 && chmod +x amm && ./amm
-
-
+curl -L -o amm https://git.io/vP4Gw && chmod +x amm
--- install-elm.sh
+++ install-elm.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
+# https://guide.elm-lang.org/get_started.html
+
+npm install -g elm
+npm install -g elm-oracle
+
 ## @see http://alphydan.svbtle.com/elm-lang-on-ubuntu-14-04
 ## @see https://github.com/elm-lang/elm-platform
 ## @notice
 ### elm-Platform/0.16/.cabal-sandbox/bin to your PATH (like this). This is necessary to successfully build elm-reactor which relies on elm-make.
 
-VERSION=0.16
+#VERSION=0.16
 
-ghc --version
+#ghc --version
 
 ##########################################################################################################
 
-sudo apt-get install libtinfo-dev
+#udo apt-get install libtinfo-dev
 
 #########################################################################################################
 
-cd ~/dev-env/
+#cd ~/dev-env/
 
 # if you are on windows, or some other place without curl, just download this file manually
-curl https://raw.githubusercontent.com/elm-lang/elm-platform/master/installers/BuildFromSource.hs > BuildFromSource.hs
+#curl https://raw.githubusercontent.com/elm-lang/elm-platform/master/installers/BuildFromSource.hs > BuildFromSource.hs
 
-runhaskell BuildFromSource.hs $VERSION
+#runhaskell BuildFromSource.hs $VERSION
 
 #########################################################################################################
-npm install -g elm-oracle
+#npm install -g elm-oracle
