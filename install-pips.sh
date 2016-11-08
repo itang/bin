596c7d10a5dbc18b911af2e67bc67ab3fb23f358
itang <live.tang@gmail.com>
2016-11-08 21:31:58 +0800

    more pips

--- install-pips.sh
+++ install-pips.sh
@@ -2,18 +2,22 @@
 
 
 ## reinstall pip
-sudo apt-get remove python-pip
-sudo apt-get autoremove
+#sudo apt-get remove python-pip
+#sudo apt-get autoremove
 
-cd /tmp
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
+#cd /tmp
+#wget https://bootstrap.pypa.io/get-pip.py
+#sudo python get-pip.py
 
-## pipsa
-sudo pip install pep8
+## pips
+pip install pep8
 
-sudo pip install ptpython
+pip install ptpython
 
-sudo pip install pyinstaller
+pip install pyinstaller
 
-sudo pip install redis
+pip install redis
+
+pip instal fabric
+
+pip install honcho
