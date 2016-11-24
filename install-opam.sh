dd128c2767fa9a3d191475dd860da9ec945511c4
itang <live.tang@gmail.com>
2016-11-24 18:42:13 +0800

    download from binary

--- install-opam.sh
+++ install-opam.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+# wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+version=1.2.2
+
+cd /tmp
+wget https://github.com/ocaml/opam/releases/download/$version/opam-$version-x86_64-Linux
+
+mv opam-$version-x86_64-Linux opam
+sudo cp opam /usr/local/bin/ && sudo chmod +x /usr/local/bin/opam
+
+opam --version
