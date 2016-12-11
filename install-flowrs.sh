4c2bbfa7cd0523eba867ca29e2053aeb8290f40f
itang <live.tang@gmail.com>
2016-12-11 21:31:03 +0800

    up

---
+++ install-flowrs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get install libncurses5-dev libncursesw5-dev libreadline-dev
+
+cargo install flow
--- install-purescript.sh
+++ install-purescript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version=0.10.2
+version=0.10.3
 
 function install_by_source() {
     cd /tmp
