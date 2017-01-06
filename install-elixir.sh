92f432883c19c56ca039b1229870e76fd4c5644f
itang <live.tang@gmail.com>
2017-01-07 03:42:40 +0800

    u

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-VERSION=1.4.0-rc.0
+VERSION=1.4.0
 (
 cd /tmp
 rm Precompiled.zip
 
-wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip
-rm -rf ~/dev-env/elixir
+wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm -rf ~/dev-env/elixir
 mkdir -p ~/dev-env/elixir
 
 unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
--- install-gradle.sh
+++ install-gradle.sh
@@ -1,13 +1,13 @@
-VERSION=3.2.1
+VERSION=3.3
 (
 cd /tmp
 
-if [ ! -f gradle-${VERSION}-all.zip ]; then
-   wget https://downloads.gradle.org/distributions/gradle-${VERSION}-all.zip
+if [ ! -f gradle-${VERSION}-bin.zip ]; then
+   wget https://downloads.gradle.org/distributions/gradle-${VERSION}-bin.zip
 fi
 
 ls gradle-${VERSION}-all.zip &&
-unzip gradle-${VERSION}-all.zip -d ~/dev-env &&
+unzip gradle-${VERSION}-bin.zip -d ~/dev-env &&
 cd ~/dev-env &&
 rm -rf gradle &&
 ln -s gradle-${VERSION} gradle &&
