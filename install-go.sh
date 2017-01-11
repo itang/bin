e1758b2b3027330a39f23b793b01589b3adbdf23
itang <live.tang@gmail.com>
2017-01-11 22:47:51 +0800

    go 1.8rc1

--- install-go.sh
+++ install-go.sh
@@ -2,7 +2,7 @@
 
 go version
 
-VERSION=1.8beta2
+VERSION=1.8rc1
 function install() {
     URL=https://storage.googleapis.com/golang/go${VERSION}.linux-amd64.tar.gz
     echo "download from ${URL}"
