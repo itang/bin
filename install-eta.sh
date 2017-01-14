713daa8f9ef0d0dbf5e5d2c559205b995fb49f97
itang <live.tang@gmail.com>
2017-01-15 00:04:57 +0800

    add install eta shell

---
+++ install-eta.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# http://eta-lang.org/docs/html/getting-started.html#method-1-source-installation
+
+cd ~/dev-env
+# http://stackoverflow.com/questions/59838/check-if-a-directory-exists-in-a-shell-script
+if [ -d "eta" ]; then
+  #(cd eta && git pull)
+  cd eta
+  ./update.sh
+else
+  git clone --recursive https://github.com/typelead/eta
+  cd eta
+  stack setup
+  ./install.sh
+fi
+
+eta --version
