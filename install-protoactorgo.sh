719bda5a794163c0a33d22628fa9cd9afed912f1
itang <live.tang@gmail.com>
2017-01-20 22:08:57 +0800

    add install protoactorgo shell

---
+++ install-protoactorgo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#go get -u -v github.com/gogo/protobuf/proto
+#go get -u -v github.com/gogo/protobuf/protoc-gen-gogo
+#go get -u -v github.com/gogo/protobuf/gogoproto
+#go get -u -v github.com/gogo/protobuf/protoc-gen-gofast
+#go get -u -v google.golang.org/grpc
+#go get -u -v github.com/gogo/protobuf/protoc-gen-gogofast
+#go get -u -v github.com/gogo/protobuf/protoc-gen-gogofaster
+#go get -u -v github.com/gogo/protobuf/protoc-gen-gogoslick
+#go get -u -v github.com/Workiva/go-datastructures/queue
+#go get -u -v github.com/emirpasic/gods/stacks/linkedliststack
+#go get -u -v github.com/orcaman/concurrent-map
+#go get -u -v github.com/AsynkronIT/gonet
+#go get -u -v github.com/hashicorp/consul/api
+#go get -u -v github.com/AsynkronIT/goconsole
+#go get -u -v github.com/emirpasic/gods/sets/hashset
+#go get -u -v github.com/serialx/hashring
+#go get -u -v github.com/couchbase/gocb
+
+go get  github.com/gogo/protobuf/proto
+go get  github.com/gogo/protobuf/protoc-gen-gogo
+go get  github.com/gogo/protobuf/gogoproto
+go get  github.com/gogo/protobuf/protoc-gen-gofast
+go get  google.golang.org/grpc
+go get  github.com/gogo/protobuf/protoc-gen-gogofast
+go get  github.com/gogo/protobuf/protoc-gen-gogofaster
+go get  github.com/gogo/protobuf/protoc-gen-gogoslick
+go get  github.com/Workiva/go-datastructures/queue
+go get  github.com/emirpasic/gods/stacks/linkedliststack
+go get  github.com/orcaman/concurrent-map
+go get  github.com/AsynkronIT/gonet
+go get  github.com/hashicorp/consul/api
+go get  github.com/AsynkronIT/goconsole
+go get  github.com/emirpasic/gods/sets/hashset
+go get  github.com/serialx/hashring
+go get  github.com/couchbase/gocb
