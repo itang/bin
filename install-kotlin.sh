7cf83428afae2c51f27b018e00897a3b1e27ba22
itang <live.tang@gmail.com>
2017-01-31 20:27:00 +0800

    cleanup

--- install-kotlin.sh
+++ install-kotlin.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-VERSION=1.0.4
+VERSION=1.0.6
 
 cd /tmp
 
 (
-wget https://github.com/JetBrains/kotlin/releases/download/$VERSION/kotlin-compiler-$VERSION.zip && \
-rm -rf ~/dev-env/kotlinc && \
-unzip kotlin-compiler-$VERSION.zip -d ~/dev-env/
-)
+  
+  wget http://github.com/JetBrains/kotlin/releases/download/v$VERSION/kotlin-compiler-$VERSION.zip && \
+  rm -rf ~/dev-env/kotlinc && \
+  unzip kotlin-compiler-$VERSION.zip -d ~/dev-env/
+) 
 
 kotlinc -version
