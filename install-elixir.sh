112e65d67a07dd6b6eb72428278d9fe38d679f78
itang <live.tang@gmail.com>
2017-01-31 17:36:56 +0800

    cleanup

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-VERSION=1.4.0
+VERSION=1.4.1
+
 (
-cd /tmp
-rm Precompiled.zip
-
-wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm -rf ~/dev-env/elixir
-mkdir -p ~/dev-env/elixir
-
-unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
+  cd /tmp
+  rm Precompiled.zip
+ 
+  wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm -rf ~/dev-env/elixir
+  mkdir -p ~/dev-env/elixir
+ 
+  unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
 )
+
