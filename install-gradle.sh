11ca4890d575f5d8d610c37397e267655e2aee16
itang <live.tang@gmail.com>
2017-02-17 12:54:37 +0800

    go 1.8

--- install-gradle.sh
+++ install-gradle.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 VERSION=3.3
+
 (
-cd /tmp
-
-if [ ! -f gradle-${VERSION}-bin.zip ]; then
-   wget https://downloads.gradle.org/distributions/gradle-${VERSION}-bin.zip
-fi
-
-ls gradle-${VERSION}-all.zip &&
-unzip gradle-${VERSION}-bin.zip -d ~/dev-env &&
-cd ~/dev-env &&
-rm -rf gradle &&
-ln -s gradle-${VERSION} gradle &&
-gradle -v
+  cd /tmp
+  
+  if [ ! -f gradle-${VERSION}-bin.zip ]; then
+     wget https://downloads.gradle.org/distributions/gradle-${VERSION}-bin.zip
+  fi
+  
+  ls gradle-${VERSION}-bin.zip &&
+  unzip gradle-${VERSION}-bin.zip -d ~/dev-env &&
+  cd ~/dev-env &&
+  rm -rf gradle &&
+  ln -s gradle-${VERSION} gradle &&
+  gradle -v
 )
+
