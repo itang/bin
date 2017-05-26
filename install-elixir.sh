8f329192a40d2de53941e912808b846ce0f0eb15
itang <live.tang@gmail.com>
2017-05-26 20:31:42 +0800

    update install elixir shell

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-VERSION=1.4.2
 
-(
-  cd /tmp
-  rm Precompiled.zip
+# link: https://elixir-lang.org/install.html
+
+cd /tmp
+
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+
+sudo apt-get update
+sudo apt-get install esl-erlang
+
+sudo apt-get install elixir
+
+
+#VERSION=1.4.4
+
+#(
+#  cd /tmp
+#  rm Precompiled.zip
  
-  wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm -rf ~/dev-env/elixir
-  mkdir -p ~/dev-env/elixir
+#  wget https://github.com/elixir-lang/elixir/releases/download/v${VERSION}/Precompiled.zip && rm -rf ~/dev-env/elixir
+#  mkdir -p ~/dev-env/elixir
  
-  unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
-)
+#  unzip Precompiled.zip -d ~/dev-env/elixir/ && ~/dev-env/elixir/bin/iex -v
+#)
 
