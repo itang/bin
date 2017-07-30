f6d977c2a513bab8193e95d36544e24e638478f5
itang <live.tang@gmail.com>
2017-07-30 17:44:43 +0800

    jiayou python version

--- jy.py
+++ jy.py
@@ -6,10 +6,13 @@
 # pip install pytoml
 
 import os
+import os.path
 #import subprocess
 import pytoml as toml
 from functional import seq
-with open('jiayou.toml', 'rb') as fin:
+toml_file = os.path.join(os.path.dirname(__file__), 'jiayou.toml')
+print "load urls from {}...".format(toml_file)
+with open(toml_file, 'rb') as fin:
     obj = toml.load(fin)
     seq(obj['urls']).for_each(lambda url: os.system('xdg-open {}'.format(url)))
     # for url in obj['urls']:
