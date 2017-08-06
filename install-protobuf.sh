16740ac75ebdbf86478b2366363ae4c31b846b12
itang <live.tang@gmail.com>
2017-08-06 12:23:54 +0800

    update install protobuf compiler

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-VERSION=3.2.0
+VERSION=3.3.0
 
 
 sudo apt-get install -y autoconf automake libtool curl make g++ unzip
 
 cd /tmp
+#hget https://github.com/google/protobuf/releases/download/v$VERSION/protobuf-cpp-$VERSION.tar.gz
+wget https://github.com/google/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip
 
-hget https://github.com/google/protobuf/releases/download/v$VERSION/protobuf-cpp-$VERSION.tar.gz
-
-tar zxvf protobuf-cpp-$VERSION.tar.gz
-
-cd protobuf-$VERSION
-
-./configure && make && make check
-
-sudo make install
-
-sudo ldconfig
+rm -rf ~/dev-env/protobuf
+unzip protoc-$VERSION-linux-x86_64.zip -d ~/dev-env/protobuf
