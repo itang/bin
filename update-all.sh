a537288b0582e672b41d1c88fc006d1b6288f920
itang <live.tang@gmail.com>
2017-08-06 08:49:42 +0800

    update my all things

---
+++ update-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+gits="$HOME/workspace/load-imei $HOME/workspace/tangcloud $HOME/workspace/tests $HOME/bin"
+for g in $gits
+do
+    (
+        echo "git pull: $g";
+        cd $g;
+        git pull
+    )
+done
+
+
+export SDKMAN_DIR="/home/itang/.sdkman"
+[[ -s "/home/itang/.sdkman/bin/sdkman-init.sh" ]] && source "/home/itang/.sdkman/bin/sdkman-init.sh"
+sdk selfupdate
+
+
+rustup update nightly 
+# cargo install --git https://github.com/ogham/exa
+cargo install-update -a
+
+
+opam update
+opam upgrade
