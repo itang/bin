9b8a5ec86123cefaa6faa7d48e2bec372ee1678d
itang <live.tang@gmail.com>
2017-10-26 08:06:32 +0800

    nginxa

--- update-all.sh
+++ update-all.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-
-gits="$HOME/workspace/load-imei $HOME/tang/load-imei $HOME/workspace/tangcloud $HOME/tang/tangcloud $HOME/tang/now $HOME/workspace/now $HOME/workspace/tests $HOME/tang/tests $HOME/bin"
-for g in $gits
-do
+gits="$HOME/workspace/load-imei $HOME/tang/load-imei $HOME/workspace/tangcloud $HOME/tang/tangcloud $HOME/tang/now $HOME/workspace/now $HOME/workspace/tests $HOME/tang/tests $HOME/workspace/nginxa $HOME/tang/nginxa $HOME/bin"
+for g in $gits; do
     (
-        echo "git pull: $g";
-        cd "$g";
+        echo "git pull: $g"
+        cd "$g"
         git pull
     )
 done
-
 
 export SDKMAN_DIR="/home/itang/.sdkman"
 [[ -s "/home/itang/.sdkman/bin/sdkman-init.sh" ]] && source "/home/itang/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
 
-
-rustup update nightly 
+rustup update nightly
 # cargo install --git https://github.com/ogham/exa
 cargo install-update -a
-
 
 opam update
 opam upgrade
