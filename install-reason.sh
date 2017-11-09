0ba2542d5cb6696665818decbfcf65c52b5de09d
itang <live.tang@gmail.com>
2017-11-09 09:42:46 +0800

    update

--- install-reason.sh
+++ install-reason.sh
@@ -2,5 +2,7 @@
 
 REASON_VERSION=3.0.1
 
-npm install -g https://github.com/reasonml/reason-cli/archive/$REASON_VERSION-bin-linux.tar.gz
 npm install -g bs-platform
+
+npm install -g https://github.com/reasonml/reason-cli/archive/$REASON_VERSION-bin-linux.tar.gz
+npm install -g upgrade-reason-syntax
