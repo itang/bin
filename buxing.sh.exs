50e0cd9e281a60e586b5c541cceceeb51bf26d58
itang <live.tang@gmail.com>
2017-11-10 17:20:18 +0800

    update

--- buxing.sh.exs
+++ buxing.sh.exs
@@ -1,14 +1,10 @@
 #! /usr/bin/env elixir 
 
 defmodule A do
-  def m(d) do
-    r = 60.0 / 5000.0
-    r * d
-  end
+  def m(d), do: 60.0 / 5000.0 * d
 end
 
 # IO.inspect(System.argv())
-
 d = (List.first(System.argv()) || "1000") |> String.to_integer()
 
 mf = A.m(d)
@@ -17,9 +13,9 @@ md = mf - mi
 
 IO.puts("")
 
-# TODO: :io.format
 if md == 0.0 do
   IO.puts("步行 #{d} 米, 需花 #{mi} 分钟\n\n")
 else
-  IO.puts("步行 #{d} 米, 需花 #{mi} 分 #{md * 60} 秒\n\n")
+  sec = :io_lib.format("~4.1f", [md * 60]) |> List.to_string()
+  IO.puts("步行 #{d} 米, 需花 #{mi} 分 #{sec} 秒\n\n")
 end
