482586bd3f48817e3295c269404d01e0f22d1e32
itang <live.tang@gmail.com>
2017-11-24 16:41:39 +0800

    scalafmt tools

---
+++ install-scalafmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -L -o ~/.local/bin/coursier https://git.io/vgvpD && chmod +x coursier
+coursier --help
+
+coursier bootstrap com.geirsson:scalafmt-cli_2.12:1.3.0 -o ~/.local/bin/scalafmt --standalone --main org.scalafmt.cli.Cli
+
+scalafmt --help
+
+coursier bootstrap ch.epfl.scala:scalafix-cli_2.12.3:0.5.3 -f --main scalafix.cli.Cli -o ~/.local/bin/scalafix
+scalafix --help
