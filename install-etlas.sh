807ca20f3eaa68f7bd92158c24c84485fd8f9a91
itang <live.tang@gmail.com>
2017-11-24 14:54:36 +0800

    update

--- install-etlas.sh
+++ install-etlas.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-cd /tmp 
-wget http://88a2a1b21f8e03a6bc8d-8f2e61d843ea88e4f30ab3f81ca0e396.r42.cf5.rackcdn.com/etlas-1.0.2.0/binaries/x86_64-linux/etlas
-cp etlas ~/.local/bin
-chmod +x ~/.local/bin/etlas
+etlas --version
+
+mode=$1
+
+if [ $mode = "install" ]; then
+    cd /tmp
+    wget http://88a2a1b21f8e03a6bc8d-8f2e61d843ea88e4f30ab3f81ca0e396.r42.cf5.rackcdn.com/etlas-1.1.0.0/binaries/x86_64-linux/etlas
+    cp etlas ~/.local/bin
+    chmod +x ~/.local/bin/etlas
+else
+    eltas update
+    eltas select latest
+fi
 
 etlas --version
