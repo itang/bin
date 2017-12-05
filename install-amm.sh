7bb91e9e2b22b41a3fa27c962cb6ff4eb143a01b
itang <live.tang@gmail.com>
2017-12-05 21:21:11 +0800

    update

--- install-amm.sh
+++ install-amm.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/.ammonite && curl -L -o ~/.ammonite/predef.sc https://git.io/vHaKQ
 
 cd ~/.local/bin
-curl -L -o amm https://git.io/v7cx7 && chmod +x amm
+curl -L -o amm https://git.io/vdNv2 && chmod +x amm
