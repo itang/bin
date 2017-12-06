42f0469f72a8ab854ae6c11a812a449e76452aab
itang <live.tang@gmail.com>
2017-12-06 13:16:59 +0800

    u

--- update-all.sh
+++ update-all.sh
@@ -2,6 +2,7 @@
 
 # set -x
 
+## gits
 gits="$HOME/workspace/load-imei $HOME/tang/load-imei $HOME/workspace/tangcloud $HOME/tang/tangcloud $HOME/tang/now $HOME/workspace/now $HOME/workspace/tests $HOME/tang/tests $HOME/workspace/nginxa $HOME/tang/nginxa $HOME/bin"
 for g in $gits; do
     (
@@ -11,10 +12,12 @@ for g in $gits; do
     )
 done
 
+## sdkman
 export SDKMAN_DIR="/home/itang/.sdkman"
 [[ -s "/home/itang/.sdkman/bin/sdkman-init.sh" ]] && source "/home/itang/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
 
+## rust
 rustup update nightly
 # cargo install --force --git https://github.com/ogham/ex
 cargo install-update -a
@@ -22,7 +25,18 @@ cargo install-update -a
 #opam update
 #opam upgrade
 
+## elixir
 (
     cd ~/dev-env/elixir
     git pull && make clean test
+)
+
+## ng
+(
+    go get -u -v neugram.io/ng
+)
+
+## goby
+(
+    go get -u -v github.com/goby-lang/goby
 )
