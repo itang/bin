69ba381ed8176828ba8a6798381adf36551153ad
itang <live.tang@gmail.com>
2017-12-27 10:27:06 +0800

    cleanup

--- update-all.sh
+++ update-all.sh
@@ -3,21 +3,36 @@
 # set -x
 
 ## gits
-gits="$HOME/workspace/load-imei $HOME/tang/load-imei $HOME/workspace/tangcloud $HOME/tang/tangcloud $HOME/tang/now $HOME/workspace/now $HOME/workspace/tests $HOME/tang/tests $HOME/workspace/nginxa $HOME/tang/nginxa $HOME/bin"
+gits="$HOME/workspace/load-imei
+$HOME/tang/load-imei
+$HOME/workspace/tangcloud
+$HOME/tang/tangcloud
+$HOME/tang/now
+$HOME/workspace/now
+$HOME/workspace/tests
+$HOME/tang/tests
+$HOME/workspace/nginxa
+$HOME/tang/nginxa
+$HOME/bin"
+
 for g in $gits; do
-    (
+    if [ -d "$g" ]; then
+        (
         echo "git pull: $g"
         cd "$g"
         git pull
-    )
+        )
+    else 
+        echo "no exists, ignore $g"
+    fi
 done
 
-## sdkman
+echo "sdkman"
 export SDKMAN_DIR="/home/itang/.sdkman"
 [[ -s "/home/itang/.sdkman/bin/sdkman-init.sh" ]] && source "/home/itang/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
 
-## rust
+echo "rust"
 rustup update nightly
 # cargo install --force --git https://github.com/ogham/ex
 cargo install-update -a
@@ -25,18 +40,11 @@ cargo install-update -a
 #opam update
 #opam upgrade
 
-## elixir
-(
-    cd ~/dev-env/elixir
-    git pull && make clean test
-)
+echo "elixir"
+(cd ~/dev-env/elixir && git pull && make clean test)
 
-## ng
-(
-    go get -u -v neugram.io/ng
-)
-
-## goby
-(
-    go get -u -v github.com/goby-lang/goby
-)
+gopackages="neugram.io/ng
+github.com/goby-lang/goby"
+for x in $gopackages; do
+    go get -u -v "$x"
+done
