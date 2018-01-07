04ddee2c4824456b562509f4161dd99f8becf36a
itang <live.tang@gmail.com>
2018-01-07 13:21:12 +0800

    more

--- data/tip/ls.md
+++ data/tip/ls.md
@@ -4,4 +4,8 @@
 
 alias ls="exa"
 
+    tree显示target目录下最多2级目录
     $ ls -lT target -L 2
+
+    创建时间降序显示文件
+    $ ls -rs created
--- update-all.sh
+++ update-all.sh
@@ -22,7 +22,7 @@ for g in $gits; do
         (
         echo "git pull: $g"
         cd "$g"
-        git pull
+        git pull &
         )
     else 
         echo "no exists, ignore $g"
@@ -30,24 +30,29 @@ for g in $gits; do
 done
 
 echo "sdkman"
+(
 export SDKMAN_DIR="/home/itang/.sdkman"
 [[ -s "/home/itang/.sdkman/bin/sdkman-init.sh" ]] && source "/home/itang/.sdkman/bin/sdkman-init.sh"
 sdk selfupdate
+) &
 
 echo "rust"
+(
 rustup update nightly
 # cargo install --force --git https://github.com/ogham/ex
 cargo install-update -a
-
+) &
 #opam update
 #opam upgrade
 
 echo "elixir"
-(cd ~/dev-env/elixir && git pull && make clean compile)
+(cd ~/dev-env/elixir && git pull && make clean compile) &
 #(cd ~/dev-env/elixir && git pull && make clean test)
 
 gopackages="neugram.io/ng
 github.com/goby-lang/goby"
 for x in $gopackages; do
-    go get -u -v "$x"
+    go get -u -v "$x" &
 done
+
+wait
