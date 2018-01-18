c25a3bf5712bc7fd0c9a617a7e6b325c83e64500
itang <live.tang@gmail.com>
2018-01-18 20:40:23 +0800

    protoc-gen-twirp

--- install-protoactorgo.sh
+++ install-protoactorgo.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 go get -u -v github.com/golang/protobuf/protoc-gen-go
+go get -u -v github.com/twitchtv/twirp/protoc-gen-twirp
 
-go get -u -v github.com/gogo/protobuf/proto
-go get -u -v github.com/gogo/protobuf/protoc-gen-gogo
-go get -u -v github.com/gogo/protobuf/gogoproto
-go get -u -v github.com/gogo/protobuf/protoc-gen-gofast
-go get -u -v google.golang.org/grpc
-go get -u -v github.com/gogo/protobuf/protoc-gen-gogofast
-go get -u -v github.com/gogo/protobuf/protoc-gen-gogofaster
-go get -u -v github.com/gogo/protobuf/protoc-gen-gogoslick
-go get -u -v github.com/Workiva/go-datastructures/queue
-go get -u -v github.com/emirpasic/gods/stacks/linkedliststack
-go get -u -v github.com/orcaman/concurrent-map
-go get -u -v github.com/AsynkronIT/gonet
-go get -u -v github.com/hashicorp/consul/api
-go get -u -v github.com/AsynkronIT/goconsole
-go get -u -v github.com/emirpasic/gods/sets/hashset
-go get -u -v github.com/serialx/hashring
-go get -u -v github.com/couchbase/gocb
+# go get -u -v github.com/gogo/protobuf/proto
+# go get -u -v github.com/gogo/protobuf/protoc-gen-gogo
+# go get -u -v github.com/gogo/protobuf/gogoproto
+# go get -u -v github.com/gogo/protobuf/protoc-gen-gofast
+# go get -u -v google.golang.org/grpc
+# go get -u -v github.com/gogo/protobuf/protoc-gen-gogofast
+# go get -u -v github.com/gogo/protobuf/protoc-gen-gogofaster
+# go get -u -v github.com/gogo/protobuf/protoc-gen-gogoslick
+# go get -u -v github.com/Workiva/go-datastructures/queue
+# go get -u -v github.com/emirpasic/gods/stacks/linkedliststack
+# go get -u -v github.com/orcaman/concurrent-map
+# go get -u -v github.com/AsynkronIT/gonet
+# go get -u -v github.com/hashicorp/consul/api
+# go get -u -v github.com/AsynkronIT/goconsole
+# go get -u -v github.com/emirpasic/gods/sets/hashset
+# go get -u -v github.com/serialx/hashring
+# go get -u -v github.com/couchbase/gocb
+# 
