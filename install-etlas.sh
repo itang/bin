7f06e5cd66896736e82cb6d51d2cdf11e976abd9
itang <live.tang@gmail.com>
2018-02-04 20:27:23 +0800

    more

--- install-etlas.sh
+++ install-etlas.sh
@@ -6,7 +6,7 @@ mode=$1
 
 if [ "$mode" = "install" ]; then
     cd /tmp
-    wget http://88a2a1b21f8e03a6bc8d-8f2e61d843ea88e4f30ab3f81ca0e396.r42.cf5.rackcdn.com/etlas-1.1.0.0/binaries/x86_64-linux/etlas
+    wget https://cdnverify.eta-lang.org/eta-binaries/etlas-1.2.0.0/binaries/x86_64-linux/etlas
     cp etlas ~/.local/bin
     chmod +x ~/.local/bin/etlas
 fi
