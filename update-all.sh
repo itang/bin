581c91a8e5ff29b90a4355706cb3513efd417fd6
itang <live.tang@gmail.com>
2018-08-31 20:39:32 +0800

    u go

--- update-all.sh
+++ update-all.sh
@@ -45,7 +45,6 @@ echo "elixir"
 gopackages="
 github.com/golang/dep/cmd/dep
 github.com/tockins/realize
-golang.org/x/vgo
 neugram.io/ng
 github.com/kode4food/sputter/cmd/sputter
 github.com/goby-lang/goby"
