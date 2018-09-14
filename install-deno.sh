618ce83f6854b1c15d7a4ddf88461231400175cf
itang <live.tang@gmail.com>
2018-09-14 11:19:11 +0800

    update deno

--- install-deno.sh
+++ install-deno.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-VERSION=v0.1.3
+VERSION=v0.1.4
 
 deno --version
 
 cd /tmp
+rm deno_linux_x64.gz
 wget https://github.com/denoland/deno/releases/download/${VERSION}/deno_linux_x64.gz
 
 gunzip  -c deno_linux_x64.gz > ~/.local/bin/deno
