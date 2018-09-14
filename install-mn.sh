209284468438baadea5f9acdf852e87c44b4ed0a
itang <live.tang@gmail.com>
2018-09-14 11:18:54 +0800

    add install micronaut

---
+++ install-mn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=1.0.0.M4
+
+cd /tmp
+wget https://github.com/micronaut-projects/micronaut-core/releases/download/v${VERSION}/micronaut-${VERSION}.zip
+
+unzip micronaut-${VERSION}.zip -d ~/dev-env
+
+cd ~/dev-env
+ln -sfn micronaut-${VERSION} micronaut
