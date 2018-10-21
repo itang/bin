245c5521005e3fa59e63eb4f60d93b78ef9bb86c
itang <live.tang@gmail.com>
2018-10-21 14:30:16 +0800

    u

--- install-deno.sh
+++ install-deno.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=v0.1.8
+VERSION=v0.1.9
 
 deno --version
 
--- install-mill.sh
+++ install-mill.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=0.2.8
+VERSION=0.3.2
 
 curl -L -o ~/.local/bin/mill https://github.com/lihaoyi/mill/releases/download/$VERSION/$VERSION && chmod +x ~/.local/bin/mill
