2f4330b4ce8096d60809d203d0f82d75f33fda59
itang <live.tang@gmail.com>
2018-11-13 14:52:55 +0800

    xargs tips

--- kill-javas.sh
+++ kill-javas.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-jps | awk '{if($2 != "Jps") print $1}' | xargs kill
+echo "before..."
+jps -lv
+
+
+jps | awk '{if($2 != "Jps") print $1}' | xargs -r kill -9
+
+echo "after..."
+jps -lv
