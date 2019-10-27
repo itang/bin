a1d4f520fdc9b4b0be67bc53a33b4a842421fda8
itang <live.tang@gmail.com>
2019-10-27 11:15:55 +0800

    deno

--- data/tip/deno.md
+++ data/tip/deno.md
@@ -2,6 +2,27 @@
 
 ## Usage
 
-    install
+    $ deno run --allow-read=file.txt script.ts
+    $ deno run --allow-write=/tmp script.ts
+    $ deno run --allow-net script.ts
+
+    $ deno info
+    $ deno info server.ts
+
+    deno install [OPTIONS] <exe_name> <SUBCOMMAND>
+    $ deno install server server.ts
     $ deno install file_server https://deno.land/std/http/file_server.ts --allow-net --allow-read
 
+    deno bundle [OPTIONS] <source_file> [out_file]
+    $ deno bundle server.ts server-with-deps.js
+    $ deno server-with-deps.js
+
+    $ deno types
+
+    deno eval [OPTIONS] <code>
+    $ deno eval 'console.log("hello")'
+
+    deno fmt [OPTIONS] <files>...
+    $ deno fmt server.ts
+
+    $ deno test server.ts
---
+++ jy_deno.ts
@@ -0,0 +1,32 @@
+import * as toml from "https://deno.land/x/std/encoding/toml.ts";
+
+function openUrl(url:string) {
+  const p = Deno.run({ args: ['x-www-browser', url] });
+  return p.status()
+}
+
+async function tomlContent(filePath: string = "/home/itang/bin/jiayou.toml"): Promise<string> {
+  const decoder = new TextDecoder("utf-8");
+  const data = await Deno.readFile("/home/itang/bin/jiayou.toml");
+  return decoder.decode(data);
+}
+
+function urlsFromToml(tomlContent: string): Array<string> {
+  const obj = toml.parse(tomlContent);
+  const urls: string[] = obj["urls"];
+  return urls;
+}
+
+const content = await tomlContent();
+const urls = urlsFromToml(content);
+for (const url of urls) {
+  openUrl(url);
+}
+/*
+const ps = (await tomlContent().then(urlsFromToml)).map(url => {
+  console.log(url);
+  return openUrl(url);
+});
+
+await Promise.all(ps);
+*/
