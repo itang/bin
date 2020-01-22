b0b3cf117c1ea9cca5f98ea3d55c2ebfbf949aa0
itang <live.tang@gmail.com>
2020-01-22 20:44:28 +0800

    u jy.py

--- jy.py
+++ jy.py
@@ -17,11 +17,12 @@ def xdg_open(url):
     os.system('xdg-open {} > /dev/null 2>&1'.format(url))
 
 
-toml_file = os.path.join(os.path.dirname(__file__), 'jiayou.toml')
-print "load urls from {}...".format(toml_file)
+TOML_FILE = 'jiayou.toml'
+toml_file = os.path.join(os.path.dirname(__file__), TOML_FILE)
+print('load urls from %s...' % toml_file)
 
-with open(toml_file, 'rb') as fin:
-    obj = toml.load(fin)
+with open(toml_file, 'rb') as f:
+    obj = toml.load(f)
     seq(obj['urls']).for_each(xdg_open)
     # for url in obj['urls']:
     #    os.system('xdg-open {}'.format(url))
