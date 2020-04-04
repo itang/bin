dc2902711a1e0d9444b341922659a01713172823
itang <live.tang@gmail.com>
2020-04-04 18:42:09 +0800

    add install rust-analyzer shell

---
+++ data/tip/zig.md
@@ -0,0 +1,5 @@
+# zig
+
+## Usage
+
+    $ zig cc -o main main.c -target x86_64-linux-musl
---
+++ install-rust-analyzer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION="2020-03-30"
+
+FROM_NAME="rust-analyzer-linux"
+TARGET="$HOME/.local/bin/rust-analyzer"
+
+cd /tmp
+rm $FROM_NAME
+wget https://github.com/rust-analyzer/rust-analyzer/releases/download/${VERSION}/${FROM_NAME}
+
+cp $FROM_NAME "${TARGET}"
+
+chmod +x "${TARGET}"
