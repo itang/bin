89604e79c2a30737bcb1c14c120c44449ed7a201
itang <live.tang@gmail.com>
2020-04-05 22:56:36 +0800

    add deno project init shell

--- denoinit.ts
+++ denoinit.ts
@@ -3,10 +3,11 @@
 if (import.meta.main) {
   const mainContent =
     `import { serve } from "https://deno.land/std@v0.39.0/http/server.ts";
+
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({ body: "Hello World\\n" });
 }
 `;
 
