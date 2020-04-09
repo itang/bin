f858e7dd093a6c27ffdbf0b99ede404ba5db408e
itang <live.tang@gmail.com>
2020-04-09 21:12:17 +0800

    u deno

--- install-deno.sh
+++ install-deno.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-VERSION=v0.31.0
+VERSION=v0.40.0
 
 deno --version
 
 cd /tmp
-rm deno_linux_x64.gz
-wget https://github.com/denoland/deno/releases/download/${VERSION}/deno_linux_x64.gz
 
-gunzip  -c deno_linux_x64.gz > ~/.local/bin/deno
+rm deno-x86_64-unknown-linux-gnu.zip
+wget https://github.com/denoland/deno/releases/download/${VERSION}/deno-x86_64-unknown-linux-gnu.zip
+
+#gunzip  -c deno_linux_x64.gz > ~/.local/bin/deno
+unzip deno-x86_64-unknown-linux-gnu.zip
+cp deno ~/.local/bin/
 chmod +x ~/.local/bin/deno
 
 deno --version
-  
