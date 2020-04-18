c14066e578659325abc8f61273d8b44ca25ab9a5
itang <live.tang@gmail.com>
2020-04-18 09:51:20 +0800

    u

--- denoinit.ts
+++ denoinit.ts
@@ -2,7 +2,7 @@
 
 if (import.meta.main) {
   const mainContent =
-    `import { serve } from "https://deno.land/std@v0.39.0/http/server.ts";
+    `import { serve } from "https://deno.land/std/http/server.ts";
 
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
